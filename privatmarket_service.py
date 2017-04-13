212df0b2c2cc81467fa03138cbf7adbacc8bfb52
portunatos <portunatos@gmail.com>
2017-04-13 15:01:52 +0300

    add tests for viewer

--- privatmarket_service.py
+++ privatmarket_service.py
@@ -4,50 +4,14 @@ from munch import munchify as privatmarket_munchify
 from selenium.common.exceptions import StaleElementReferenceException
 from datetime import datetime, timedelta
 from pytz import timezone
-from dateutil import parser
-from selenium import webdriver
-
-def get_month_number(month_name):
-    monthes = [u"янв.", u"февр.", u"марта", u"апр.", u"мая", u"июня",
-               u"июля", u"авг.", u"сент.", u"окт.", u"нояб.", u"дек.",
-               u"січ.", u"лют.", u"бер.", u"квіт.", u"трав.", u"черв.",
-               u"лип.", u"серп.", u"вер.", u"жовт.", u"лист.", u"груд.",
-               u"січня", u"лютого", u"березня", u"квітня", u"травня", u"червня",
-               u"липня", u"серпня", u"вересня", u"жовтня", u"листопада", u"грудня"]
-    return monthes.index(month_name) % 12 + 1
 
 
-def read_file_content(file_path):
-    with open(file_path, 'r') as content_file:
-        return content_file.read()
-
-
-def fill_file_data(url, title, date_modified, date_published):
-    return privatmarket_munchify({
-        "data": {
-            "url": url,
-            "title": title,
-            "id": "",
-            "dateModified": date_modified,
-            "datePublished": date_published,
-        }
-    })
-
-
-def get_bid_data(status):
-    return privatmarket_munchify({
-        "data": {
-            "status": status
-        }
-    })
-
-
-def is_element_not_stale(web_element):
-    try:
-        web_element.is_enabled()
-    except StaleElementReferenceException:
-        return True
-    return False
+def modify_test_data(initial_data):
+    #set user name
+    # initial_data['procuringEntity']['name'] = u'Товариство З Обмеженою Відповідальністю \'Мак Медіа Прінт\''
+    initial_data['procuringEntity']['name'] = u'Товариство З Обмеженою Відповідальністю \'Сільськогосподарська Фірма \'Рубіжне\''
+    # initial_data['procuringEntity']['name'] = u'Макстрой Діск, Товариство З Обмеженою Відповідальністю'
+    return initial_data
 
 
 def get_currency_type(currency):
@@ -63,16 +27,14 @@ def get_currency_type(currency):
         return currency
 
 
-def get_classification_type(classifications):
-    classifications_dictionary = {
-        u'ДК 016:2010': u'ДКПП',
-        u'ДК 021:2015': u'CPV'
-    }
-    classifications_type = classifications_dictionary.get(classifications)
-    if classifications_type:
-        return classifications_type
-    else:
-        return classifications
+def get_month_number(month_name):
+    monthes = [u"янв.", u"февр.", u"марта", u"апр.", u"мая", u"июня",
+               u"июля", u"авг.", u"сент.", u"окт.", u"нояб.", u"дек.",
+               u"січ.", u"лют.", u"бер.", u"квіт.", u"трав.", u"черв.",
+               u"лип.", u"серп.", u"вер.", u"жовт.", u"лист.", u"груд.",
+               u"січня", u"лютого", u"березня", u"квітня", u"травня", u"червня",
+               u"липня", u"серпня", u"вересня", u"жовтня", u"листопада", u"грудня"]
+    return monthes.index(month_name) % 12 + 1
 
 
 def get_time_with_offset(date):
@@ -82,41 +44,34 @@ def get_time_with_offset(date):
     return localized_date.strftime('%Y-%m-%d %H:%M:%S.%f%z')
 
 
-def get_procurement_method_type(method_name):
-    type_dictionary = {
-                       u'Допороговая закупка': 'belowThreshold',
-                       u'Открытые торги': 'aboveThresholdUA',
-                       u'Открытые торги с публикацией на англ. языке': 'aboveThresholdEU',
-                       u'Отчет о заключении договора': 'reporting',
-                       u'Переговорная процедура': 'negotiation',
-                       u'Срочная переговорная процедура': 'negotiation.quick',
-                       u'Открытые торги (особенности обороны)': 'aboveThresholdUA.defense'
-                       }
-    type_name = type_dictionary.get(method_name)
-    return type_name
-
-
-def get_identification_scheme(scheme):
-    scheme_dictionary = {
-        u'ЕГРПОУ': u'UA-EDR',
-        u'ЄДРПОУ': u'UA-EDR'
+def get_unit_code(name):
+    dictionary = {
+        u'кілограми': u'KGM',
+        u'пара': u'PR',
+        u'літр': u'LTR',
+        u'набір': u'SET',
+        u'пачок': u'NMP',
+        u'метри': u'MTR',
+        u'лот': u'LO',
+        u'послуга': u'E48',
+        u'метри кубічні': u'MTQ',
+        u'ящик': u'BX',
+        u'рейс': u'E54',
+        u'тони': u'TNE',
+        u'метри квадратні': u'MTK',
+        u'кілометри': u'KMT',
+        u'штуки': u'H87',
+        u'місяць': u'MON',
+        u'пачка': u'RM',
+        u'упаковка': u'PK',
+        u'гектар': u'HAR',
+        u'блок': u'D64'
     }
-    identification_scheme = scheme_dictionary.get(scheme)
-    if identification_scheme:
-        return identification_scheme
+    expected_name = dictionary.get(name)
+    if expected_name:
+        return expected_name
     else:
-        return scheme
-
-
-def get_doc_identifier(doc_type_name):
-    type_dictionary = {
-                       'eligibility_documents': 20,
-                       'qualification_documents': u'Підтвердження відповідності кваліфікаційним критеріям',
-                       'documents': u'Документи, що підтверджують відповідність',
-                       'financial_documents': 49
-                       }
-    type_name = type_dictionary.get(doc_type_name)
-    return str(type_name)
+        return name
 
 
 def get_unit_name(current_name):
@@ -157,34 +112,17 @@ def get_unit_name(current_name):
         return current_name
 
 
-def get_unit_code(name):
-    dictionary = {
-        u'кілограми': u'KGM',
-        u'пара': u'PR',
-        u'літр': u'LTR',
-        u'набір': u'SET',
-        u'пачок': u'NMP',
-        u'метри': u'MTR',
-        u'лот': u'LO',
-        u'послуга': u'E48',
-        u'метри кубічні': u'MTQ',
-        u'ящик': u'BX',
-        u'рейс': u'E54',
-        u'тони': u'TNE',
-        u'метри квадратні': u'MTK',
-        u'кілометри': u'KMT',
-        u'штуки': u'H87',
-        u'місяць': u'MON',
-        u'пачка': u'RM',
-        u'упаковка': u'PK',
-        u'гектар': u'HAR',
-        u'блок': u'D64'
+def get_classification_type(classifications):
+    classifications_dictionary = {
+        u'ДК 016:2010': u'ДКПП',
+        u'ДК 021:2015': u'CPV',
+        u'ДК 18-2000': u'ДК018'
     }
-    expected_name = dictionary.get(name)
-    if expected_name:
-        return expected_name
+    classifications_type = classifications_dictionary.get(classifications)
+    if classifications_type:
+        return classifications_type
     else:
-        return name
+        return classifications
 
 
 def get_status_type(status_name):
@@ -208,67 +146,3 @@ def get_status_type(status_name):
                        }
     type_name = type_dictionary.get(status_name)
     return type_name
-
-
-def modify_test_data(initial_data):
-    # set enquiryPeriod.startDate
-    # enquiryPeriod_startDate = parser.parse(initial_data['enquiryPeriod']['startDate'])
-    # enquiryPeriod_startDate = enquiryPeriod_startDate + timedelta(minutes=6)
-    # initial_data["enquiryPeriod"]["startDate"] = enquiryPeriod_startDate.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
-    #
-    # # set lots
-    # initial_data['lots'] = [privatmarket_munchify(
-    # {
-    #     "description": u'Тестовий лот',
-    #     "title": initial_data['title'],
-    #     "value": {
-    #         "currency": "UAH",
-    #         "amount": initial_data['value']['amount'],
-    #         "valueAddedTaxIncluded": True
-    #     },
-    #     "minimalStep": {
-    #         "currency": "UAH",
-    #         "amount": initial_data['minimalStep']['amount'],
-    #         "valueAddedTaxIncluded": True
-    #     },
-    #     "status": "active"
-    # })]
-
-    # set other
-    # initial_data['procuringEntity']['name'] = u'Товариство З Обмеженою Відповідальністю \'Мак Медіа Прінт\''
-    initial_data['procuringEntity']['name'] = u'Товариство З Обмеженою Відповідальністю \'Сільськогосподарська Фірма \'Рубіжне\''
-    # initial_data['procuringEntity']['name'] = u'Макстрой Діск, Товариство З Обмеженою Відповідальністю'
-    #initial_data['items'][0]['unit']['name'] = get_unit_ru_name(initial_data['items'][0]['unit']['name'])
-    return initial_data
-
-
-def get_unit_ru_name(name):
-    dictionary = {
-        u'кілограми': u'килограмм',
-        u'кілометер': u'километр',
-        u'пара': u'пара',
-        u'літр': u'литр',
-        u'набір': u'набор',
-        u'пачок': u'пачка',
-        u'метри': u'метр',
-        u'послуга': u'услуга',
-        u'метри кубічні': u'метр кубический',
-        u'тони': u'тонна',
-        u'метри квадратні': u'метр квадратный',
-        u'кілометри': u'километр',
-        u'штуки': u'штука',
-        u'місяць': u'месяц',
-        u'пачка': u'пачка',
-        u'упаковка': u'упаковка',
-        u'гектар': u'гектар',
-        u'блок': u'блок'
-    }
-    expected_name = dictionary.get(name)
-    if expected_name:
-        return expected_name
-    else:
-        return name
-
-
-def convert_float_to_string(number):
-    return format(number, '.2f')
